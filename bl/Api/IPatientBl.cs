d3dab62ce521a062a7ffa1f16084ae53574c9606
elisheva-wiess <S0504156200@gmail.com>
2025-04-23 17:19:59 +0300

    Update IPatientBl.cs

--- bl/Api/IPatientBl.cs
+++ bl/Api/IPatientBl.cs
@@ -11,17 +11,14 @@ namespace Bl.Api
     public interface IPatientBl
     {
         List<BlSpecializationsTherapists> GetSpecializationsTherapistsByName(string name);
-<<<<<<< HEAD
+
         List<BlSpecializations> GetAllSpecializations();
-=======
-<<<<<<< HEAD
+
 
         List<BlAvailableAppointment> ViewTherapistsAvailableDays(string name, string specializationName);
-=======
+
         BlPatient LogIn(int id);
         BlPatient SingUp(Patient patient);
 
->>>>>>> e110df938693e04e2103196cbb861ff195d715db
->>>>>>> 4b377af2ba8d08fc82a199a2fbafff5b1c71c153
     }
 }
