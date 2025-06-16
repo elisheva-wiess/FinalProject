9930ec13d644d97a22fb3cf214c4006e7d025a22
Devora-Berkowitz <0583265797d@gmail.com>
2025-06-16 22:31:56 +0300

    Upload keshev-focus-hub

--- WebProject/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ WebProject/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d670c364bfb9963e960b758ecd4ecfc8727668")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb18095120e686d68474a4e022bfc7c796b4538f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
